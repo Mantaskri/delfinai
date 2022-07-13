1b12f08d65908ee31d728cb05a304e259d243a13
Mantas Krinickas <mantas.krinickas@hotmail.com>
2022-07-13 16:29:32 +0300

    innitial commit

---
+++ 001/app.js
@@ -0,0 +1,13 @@
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+  let array = [];
+
+  for (let i = 0; i < 10; i++) {
+      array.push(rand(7, 77))
+  };
+  
+  console.log(array); 
