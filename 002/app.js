39d95ca1741d846800a97d46779e28fc1aeb0ae4
Mantas Krinickas <mantas.krinickas@hotmail.com>
2022-07-14 15:35:18 +0300

    forEach

---
+++ 002/app.js
@@ -0,0 +1,31 @@
+//kintamuju ispausdinimas su forEach :
+
+[1,5,77].forEach(a => console.log(a));
+
+console.log('-------------------------------------------');
+
+//jei norim skaiciaus didesnio uz 4 :
+
+[1,5,77].forEach(a => {
+    if (a > 4) {
+        console.log(a);
+    }
+});
+
+console.log('-------------------------------------------');
+
+// i - indexas (masyve esancio kintamojo vieta) :
+
+[1,5,77].forEach((a, i) => console.log(a, i));
+
+console.log('-------------------------------------------');
+
+// t - visas masyvas per kuri pereidinejam
+
+[1, 5, 77].forEach((a, i, t) => console.log(t[2]));
+
+console.log('-------------------------------------------');
+
+//spausdinimas is kitos puses, apverciant
+
+[1, 5, 77].forEach((a, i, t) => console.log(t[t.length - i - 1]));
