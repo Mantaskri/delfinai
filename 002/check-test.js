aa1bf856047264b4e768666394dcb5f76e06af85
Mantas Krinickas <mantas.krinickas@hotmail.com>
2022-07-17 20:10:07 +0300

    1uzd.

--- 002/check-test.js
+++ 002/check-test.js
@@ -3,5 +3,13 @@ const girls = ['Brigita', 'Edita', 'Deimante', 'Juste', 'Ona', 'Brone',
  'Ramune', 'Kastone', 'Meilute'];
 console.log(girls);
  // 1
-
- const cats = [] 
+ console.log('----------1-----------');
+ 
+ function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+ }
+    const cats = [...Array(10)].map(_ => rand(2, 12));
+    const owners = [...Array(10)].map(_ => girls[rand(0, 8)])
+    console.log(cats, owners);
