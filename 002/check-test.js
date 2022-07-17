71d5d3691485f698e4e5ae6f81eb96406f44cc9e
Mantas Krinickas <mantas.krinickas@hotmail.com>
2022-07-17 20:05:24 +0300

    innit test

---
+++ 002/check-test.js
@@ -0,0 +1,7 @@
+console.log('-----------------------------------Testas------------------------------------');
+const girls = ['Brigita', 'Edita', 'Deimante', 'Juste', 'Ona', 'Brone',
+ 'Ramune', 'Kastone', 'Meilute'];
+console.log(girls);
+ // 1
+
+ const cats = [] 
