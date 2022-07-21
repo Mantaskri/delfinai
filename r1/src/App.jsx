893b2fc2c32fc8e850e392dbe9c30a599546d2dc
Mantas Krinickas <mantas.krinickas@hotmail.com>
2022-07-21 13:47:04 +0300

    buttons

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,70 +1,15 @@
 import './App.css';
-import Divider from './components/005/Divider';
-import DogsLowerC from './components/005/DogsLowerC';
-import Dogs from './components/005/Dogs';
-import DogsRound from './components/005/DogsRound';
-import DogsSize from './components/005/DogsSize';
-import DogsSq from './components/005/DogsSq';
-import Labas from './components/005/Labas';
-import OneProps from './components/005/OneProps';
-import TreeProps from './components/005/TreeProps';
-import Texts from './components/005/Texts';
-import Zebrai from './components/005/Zebrai';
-import rand from './Functions/rand';
-;
+import Fun from './components/006/Fun';
 
-// const ms = { color: 'red', backgroundColor: 'white', padding: '20px'} 
-
-const home1 = 'basic'
-const home2 = 'list'
-
-const txt1 = 'some blah'
-const txt2 = 'some blah blah'
-
-const dogs = ['šuo', 'šunius', 'Bobikas', 'kudlius', 'Šarikas', 'avigalvis'];
-const kvadratas = {marginTop: '20px', width: "150px", minHeight: '150px', border: '2px solid white', textAlign: 'center', display: 'flex', justifyContent: 'center', alignItems: 'center'}
-const apsktitimas = {marginTop: '20px', width: "200px", minHeight: '200px', border: '2px solid white', borderRadius: '50%', textAlign: 'center', display: 'flex', justifyContent: 'center', alignItems: 'center'}
-
-function App() { // privaloma komponento pavadinima rasyti is didziosios raides!; rekomendacija deti komponentus i atskirus tuo paciu pavadinimu!; komponentas privalo grazinti html;
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Divider name={home1} nr='1'/>
-        <Labas/>
-        <Divider name={home1} nr='2'/>
-        <OneProps text={'Bet koks tekstas'}/>
-        <Divider name={home1} nr='3'/>
-        <Zebrai colnr={rand(1,2)}/>
-        <Divider name={home1} nr='4'/>
-        <Texts t1={txt1} t2={txt2}/>
-        <Divider name={home1} nr='5'/>
-        <TreeProps t1={txt1} t2={txt2} col='yellow'/>
-        {/* <Kosmosas spalva='pink'/>
-        <Kosmosas manoStilius = {ms}/> */}
-        <Divider name={home2} nr='1'/>
-        {
-          dogs.map((n,i) => <DogsSq key={i} name={n} stilius={kvadratas}></DogsSq>)
-        }
-        <Divider name={home2} nr='2'/>
-        {
-          [...dogs].sort((a,b) => b.length-a.length).map((n,i) => <DogsRound key={i+1} name={n} stilius={apsktitimas} nr={i+1}></DogsRound>)
-        }
-        <Divider name={home2} nr='3'/>
-        {
-          dogs.map((n,i) => <Dogs key={i} name={n} kv1={kvadratas} rn1={apsktitimas} nr={i}></Dogs>)
-        }
-        <Divider name={home2} nr='4'/>
-        {
-          dogs.map((n,i) => <DogsLowerC key={i} name={n}/>)
-        }
-        <Divider name={home2} nr='5'/>
-        {
-          dogs.map((n,i) => <DogsSize key={i} name={n}/>)
-        }
-      </header>
-    </div>
-  );
+function App() {
+    return (
+        <div className="App">
+            <header className='App-header'>
+                <h1>State</h1>
+                    <Fun spalva='red'></Fun>
+            </header>
+        </div>
+    );
 }
-//<Kosmosas spalva='pink'/> atributai gali buti surasyti per tarpa pvz <Kosmosas spalva='pink' padding = '40px'/>
 
 export default App;
---
+++ r1/src/components/006/Fun.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+function Fun({spalva}) {
+
+    const [bg, setBg] = useState('green');
+    const [count, setCount] =useState(0);
+
+    const changeB = () => {
+      //  setBg('blue');
+        setBg(b => b === 'green' ? 'blue' : 'green');
+        console.log(bg);
+    }
+
+    const addOne = () => {
+        setCount(c => c + 1);
+    }
+
+    return (
+        <>
+    <h2 style={{
+        color: spalva,
+        backgroundColor: bg,
+        borderRadius: 50,
+        
+    }}>FUN {count}</h2>
+    <button onClick={changeB}>Do Blue</button>
+    <button onClick={addOne}>+ 1</button>
+    </>
+    );
+}
+export default Fun;
